78d4e7426aeecef9365c2fd1f32b0e4d65cd6329
Tanath <3394708+Tanath@users.noreply.github.com>
2020-09-08 13:04:10 -0400

    Add zplug setup file with autosuggestions plugin.

---
+++ .zplug.zsh
@@ -0,0 +1,16 @@
+[[ -f ~/.zplug/init.zsh ]] && export ZPLUG_HOME=~/.zplug && source ~/.zplug/init.zsh
+# git clone https://github.com/zplug/zplug $ZPLUG_HOME
+
+# Fish-like autosuggestions for zsh
+zplug "zsh-users/zsh-autosuggestions"
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
+
+# Then, source plugins and add commands to $PATH
+zplug load
