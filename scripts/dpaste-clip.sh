2804a68f991d1a6d7246f8497954ecf399d7e8d8
Tanath <3394708+Tanath@users.noreply.github.com>
2020-09-21 22:47:55 -0400

    Add failure notification.

--- scripts/dpaste-clip.sh
+++ scripts/dpaste-clip.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-xclip -o -sel clip | curl -s -F "content=<-" https://dpaste.com/api/ | xclip -sel clip
-notify-send -t 2500 "dpaste.com" "URL copied to clipboard"
+xclip -o -sel clip | curl -sF "content=<-" https://dpaste.com/api/ | xclip -sel clip && \
+    notify-send -t 2500 "dpaste.com" "URL copied to clipboard" || \
+    notify-send -t 2500 "dpaste.com" "Clipboard dpaste failed"
--- scripts/dpaste-sel.sh
+++ scripts/dpaste-sel.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-xclip -o | curl -s -F "content=<-" https://dpaste.com/api/ | xclip -sel primary
-notify-send -t 2500 "dpaste.com" "URL copied to selection"
+xclip -o | curl -sF "content=<-" https://dpaste.com/api/ | xclip -sel primary && \
+    notify-send -t 2500 "dpaste.com" "URL copied to selection" || \
+    notify-send -t 2500 "dpaste.com" "Selection dpaste failed"
