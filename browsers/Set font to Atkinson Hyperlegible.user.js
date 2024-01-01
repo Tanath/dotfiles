305f0b3263457b6c3954951a1a8df008c15cbd9a
Mike Smith <3394708+Tanath@users.noreply.github.com>
2024-01-01 01:23:01 -0500

    add td element

--- browsers/Set font to Atkinson Hyperlegible.user.js
+++ browsers/Set font to Atkinson Hyperlegible.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Set font to Atkinson Hyperlegible
-// @version      0.4.1
+// @version      0.4.2
 // @description  Set font to Atkinson Hyperlegible.
 // @author       Tanath
 // @updateURL https://github.com/Tanath/dotfiles/raw/master/browsers/Set%20font%20to%20Atkinson%20Hyperlegible.user.js
@@ -13,7 +13,7 @@
     'use strict';
     GM_addStyle(`
         @import url('https://fonts.googleapis.com/css2?display=swap&family=Atkinson+Hyperlegible');
-        body, h1, h2, h3, main, div, span, p, li, a {
+        body, h1, h2, h3, main, div, span, p, td, li, a {
             font-family: 'Atkinson Hyperlegible', sans-serif !important;
         }
     `);
