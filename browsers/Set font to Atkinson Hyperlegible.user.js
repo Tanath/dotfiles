d1dadadbb0da9643fd40e9ce778b183dc0cbfad3
Mike Smith <3394708+Tanath@users.noreply.github.com>
2024-01-03 00:02:43 -0500

    use Noto sans as fallback
    Eliminate tofu

--- browsers/Set font to Atkinson Hyperlegible.user.js
+++ browsers/Set font to Atkinson Hyperlegible.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Set font to Atkinson Hyperlegible
-// @version      0.5
+// @version      0.6
 // @description  Set font to Atkinson Hyperlegible.
 // @author       Tanath
 // @downloadURL  https://github.com/Tanath/dotfiles/raw/master/browsers/Set%20font%20to%20Atkinson%20Hyperlegible.user.js
@@ -12,16 +12,20 @@
 
 (function() {
     'use strict';
-    // Preload the font for improved performance
-    var link = document.createElement('link');
-    link.href = 'https://fonts.googleapis.com/css2?display=swap&family=Atkinson+Hyperlegible';
-    link.rel = 'preload';
-    link.as = 'font';
-    document.head.appendChild(link);
+    var link1 = document.createElement('link');
+    link1.href = 'https://fonts.googleapis.com/css2?display=swap&family=Atkinson+Hyperlegible';
+    link1.rel = 'stylesheet';
+    document.head.appendChild(link1);
+
+    var link2 = document.createElement('link');
+    link2.href = 'https://fonts.googleapis.com/css2?display=swap&family=Noto+Sans';
+    link2.rel = 'stylesheet';
+    document.head.appendChild(link2);
 
     var style = `
-        body, h1, h2, h3, main, div, span, p, td, li, a {
-            font-family: 'Atkinson Hyperlegible', sans-serif !important;
+        body, h1, h2, h3, main, article, article-body, section, div, span, p, td, li, a {
+            font-family: 'Atkinson Hyperlegible', 'Noto Sans', sans-serif !important;
         }
     `;
+    GM_addStyle(style);
 })();
