631f9e28274ce560d933d3f0f6856cd2f3a79fa6
Tanath <3394708+Tanath@users.noreply.github.com>
2024-01-20 17:49:51 -0500

    @userscript Put Source Code Pro before Fira Code due to ligatures.

--- browsers/Set font to Atkinson Hyperlegible.user.js
+++ browsers/Set font to Atkinson Hyperlegible.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Set font to Atkinson Hyperlegible
-// @version      0.8
+// @version      0.8.1
 // @description  Set font to Atkinson Hyperlegible.
 // @author       Tanath
 // @downloadURL  https://github.com/Tanath/dotfiles/raw/master/browsers/Set%20font%20to%20Atkinson%20Hyperlegible.user.js
@@ -18,21 +18,13 @@
     document.head.appendChild(link1);
 
     var link2 = document.createElement('link');
-    link2.href = 'https://fonts.googleapis.com/css2?display=swap&family=Atkinson+Hyperlegible&family=Fira+Code&family=Source+Code+Pro&family=Noto+Sans+Symbols';
+    link2.href = 'https://fonts.googleapis.com/css2?display=swap&family=Atkinson+Hyperlegible&family=Source+Code+Pro&family=Fira+Code&family=Noto+Sans+Symbols';
     link2.rel = 'stylesheet';
     document.head.appendChild(link2);
 
-/*
-    var style = `
-        body, h1, h2, h3, main, article, .article, article-body, section, div, span, p, td, li, a {
-            font-family: 'Atkinson Hyperlegible', 'Fira Code', 'Noto Sans Symbols', sans-serif;
-        }
-    `;
-*/
-
     var style = `
         * {
-            font-family: 'Atkinson Hyperlegible', 'Fira Code', 'Source Code Pro', 'Noto Sans Symbols', sans-serif;
+            font-family: 'Atkinson Hyperlegible', 'Source Code Pro', 'Fira Code', 'Noto Sans Symbols', sans-serif;
         }
     `;
     var styleElement = document.createElement('style');
