947e7169a3b0a95af8cc955aea2e4553e6a92d10
Tanath <3394708+Tanath@users.noreply.github.com>
2024-01-22 15:48:07 -0500

    @zsh @mobile: Exit if not interactive. Add ssh stuff, commented out.

--- .zmobile.zsh
+++ .zmobile.zsh
@@ -1,3 +1,10 @@
+# Exit if not interactive shell
+[[ $- != *i* ]] && return
+
 [[ -n ${commands[tsudo]} ]] && alias sudo='tsudo '         # For Termux
 [[ -n ${commands[ag]} ]] && lg () { tsudo ag $* /var/log/ } || lg () { tsudo grep $GPARAM -ir $* /var/log/* } # log grep
 
+## ssh
+#/data/data/com.termux/files/usr/bin/keychain --nolock ~/.ssh/ssh_host_ed25519_key
+#ssh-add ~/.ssh/ssh_host_ed25519_key
+#source ~/.ssh-agent > /dev/null
