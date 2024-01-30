3e770dc05c7635190f37f61d27da3c329284b48f
Tanath <3394708+Tanath@users.noreply.github.com>
2024-01-29 22:43:56 -0500

    @browsers @vim Refactor limited-vimkeys to change keys easily.

--- browsers/limited-vimkeys.user.js
+++ browsers/limited-vimkeys.user.js
@@ -3,70 +3,90 @@
 // @match       https://*/*
 // @match       http://*/*
 // @grant       window.close
-// @version     0.2
+// @version     0.2.1
 // @author      Tanath
 // @description Add vim keys for some basic functions like scrolling.
 // @downloadURL https://github.com/Tanath/dotfiles/raw/master/browsers/limited-vimkeys.user.js
 // @updateURL   https://github.com/Tanath/dotfiles/raw/master/browsers/limited-vimkeys.user.js
 // ==/UserScript==
 
+const scrollDown = 'j';
+const scrollUp = 'k';
+const scrollDownHalf = 'd';
+const scrollUpHalf = 'e';
+const downSmall = 'J';
+const upSmall = 'K';
+const jumpTop = 'g';
+const jumpBottom = 'G';
+const reload = 'r';
+const close = 'x';
+const copyURL = 'y';
+const goBack = 'S';
+const goForward = 'D';
+const upURL = 'u';
+const slower = '[';
+const faster = ']';
+const normalSpeed = '=';
+const prevSection = '{';
+const nextSection = '}';
+
 document.addEventListener('keydown', function(e) {
   if (e.target.tagName.toLowerCase() === 'input' || e.target.tagName.toLowerCase() === 'textarea') {
     return;
   }
   switch (e.key) {
-    case 'd':
-      window.scrollBy({left: 0, top: window.innerHeight * 0.4, behavior: "instant"});
-      break;
-    case 'e':
-      window.scrollBy({left: 0, top: -window.innerHeight * 0.4, behavior: "instant"});
-      break;
-    case 'j':
+    case scrollDown:
       window.scrollBy({left: 0, top: window.innerHeight * 0.25, behavior: "instant"});
       break;
-    case 'k':
+    case scrollUp:
       window.scrollBy({left: 0, top: -window.innerHeight * 0.25, behavior: "instant"});
       break;
-    case 'J':
+    case scrollDownHalf:
+      window.scrollBy({left: 0, top: window.innerHeight * 0.4, behavior: "instant"});
+      break;
+    case scrollUpHalf:
+      window.scrollBy({left: 0, top: -window.innerHeight * 0.4, behavior: "instant"});
+      break;
+    case downSmall:
       window.scrollBy({left: 0, top: window.innerHeight * 0.1, behavior: "instant"});
       break;
-    case 'K':
+    case upSmall:
       window.scrollBy({left: 0, top: -window.innerHeight * 0.1, behavior: "instant"});
       break;
-    case 'g':
+    case jumpTop:
       window.scrollTo({left: 0, top: 0, behavior: "instant"});
       break;
-    case 'G':
+    case jumpBottom:
       window.scrollTo({left: 0, top: document.body.scrollHeight, behavior: "instant"});
       break;
-    case 'r':
+    case reload:
       location.reload();
       break;
-    case 'x':
+    case close:
       window.close();
       break;
-    case 'y':
+    case copyURL:
       navigator.clipboard.writeText(window.location.href);
       break;
-    case 'S':
+    case goBack:
       history.back();
       break;
-    case 'D':
+    case goForward:
       history.forward();
       break;
-    case 'u':
+    case upURL:
       window.location.href = window.location.href.replace(/\/[^\/]+\/?$/, '');
       break;
-    case '[':
+    case slower:
       document.querySelector('video').playbackRate-=0.25
       break;
-    case ']':
+    case faster:
       document.querySelector('video').playbackRate+=0.25
       break;
-    case '=':
+    case normalSpeed:
       document.querySelector('video').playbackRate=1
       break;
-    case '{':
+    case prevSection:
       (function() {
         var prevTargetElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, hr, section, article, aside, nav, header, main');
         var currentY = window.scrollY;
@@ -79,7 +99,7 @@ document.addEventListener('keydown', function(e) {
         }
       })();
       break;
-    case '}':
+    case nextSection:
       (function() {
         var nextTargetElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, hr, section, article, aside, nav, header, main');
         var currentY = window.scrollY;
