121d4d414a3b93083f511d09bf67cb955b6bb6b8
Tanath <3394708+Tanath@users.noreply.github.com>
2024-02-01 02:06:14 -0500

    @browsers @vim: Rewrite vimkeys userscript and add repeatAction on dot.

--- browsers/limited-vimkeys.user.js
+++ browsers/limited-vimkeys.user.js
@@ -3,7 +3,7 @@
 // @match       https://*/*
 // @match       http://*/*
 // @grant       window.close
-// @version     0.3
+// @version     1.0
 // @author      Tanath
 // @description Add vim keys for some basic functions like scrolling.
 // @downloadURL https://github.com/Tanath/dotfiles/raw/master/browsers/limited-vimkeys.user.js
@@ -30,96 +30,83 @@ const faster = ']';
 const normalSpeed = '=';
 const prevSection = '{';
 const nextSection = '}';
+const repeatAction = '.';
+let lastAction = null;
+
+const actions = {
+    [scrollDown]: () => window.scrollBy({ left: 0, top: window.innerHeight * 0.25, behavior: "instant" }),
+    [scrollUp]: () => window.scrollBy({ left: 0, top: -window.innerHeight * 0.25, behavior: "instant" }),
+    [scrollDownHalf]: () => window.scrollBy({left: 0, top: window.innerHeight * 0.4, behavior: "instant"}),
+    [scrollUpHalf]: () => window.scrollBy({left: 0, top: -window.innerHeight * 0.4, behavior: "instant"}),
+    [downSmall]: () => window.scrollBy({left: 0, top: window.innerHeight * 0.1, behavior: "instant"}),
+    [upSmall]: () => window.scrollBy({left: 0, top: -window.innerHeight * 0.1, behavior: "instant"}),
+    [jumpTop]: () => window.scrollTo({left: 0, top: 0, behavior: "instant"}),
+    [jumpBottom]: () => window.scrollTo({left: 0, top: document.body.scrollHeight, behavior: "instant"}),
+    [reload]: () => location.reload(),
+    [close]: () => window.close(),
+    [copyURL]: () => navigator.clipboard.writeText(window.location.href),
+    [goBack]: () => history.back(),
+    [goForward]: () => history.forward(),
+    [upURL]: () => window.location.href = window.location.href.replace(/\/[^\/]+\/?$/, ''),
+    [focusInput]: () => {
+        let input = document.querySelector('input, textarea');
+        if (input) {
+            input.focus();
+            input.addEventListener('beforeinput', (event) => {
+                if (event.inputType === 'insertText' && event.data === 'i') {
+                    event.preventDefault();
+                }
+            });
+        }
+    },
+    [slower]: () => document.querySelector('video').playbackRate -= 0.25,
+    [faster]: () => document.querySelector('video').playbackRate += 0.25,
+    [normalSpeed]: () => document.querySelector('video').playbackRate = 1,
+    [prevSection]: (e) => {
+        (function(e) {
+            var prevTargetElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, hr, section, article, aside, nav, header, main');
+            var currentY = window.scrollY;
+            for (var i = prevTargetElements.length - 1; i >= 0; i--) {
+                var pElementY = prevTargetElements[i].getBoundingClientRect().top + window.scrollY;
+                if (pElementY < currentY) {
+                    window.scrollTo({ top: pElementY, behavior: 'smooth' });
+                    break;
+                }
+            }
+        })(e);
+    },
+    [nextSection]: (e) => {
+        (function(e) {
+            var nextTargetElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, hr, section, article, aside, nav, header, main');
+            var currentY = window.scrollY;
+            for (var i = 0; i < nextTargetElements.length; i++) {
+                var nElementY = nextTargetElements[i].getBoundingClientRect().top + window.scrollY;
+                if (nElementY > (currentY+1)) {
+                    window.scrollTo({ top: nElementY, behavior: 'smooth', block: 'start' });
+                    break;
+                }
+            }
+        })(e);
+    }
+};
 
 document.addEventListener('keydown', function(e) {
     if (e.target.tagName.toLowerCase() === 'input' || e.target.tagName.toLowerCase() === 'textarea') {
         return;
     }
-    switch (e.key) {
-        case scrollDown:
-            window.scrollBy({left: 0, top: window.innerHeight * 0.25, behavior: "instant"});
-            break;
-        case scrollUp:
-            window.scrollBy({left: 0, top: -window.innerHeight * 0.25, behavior: "instant"});
-            break;
-        case scrollDownHalf:
-            window.scrollBy({left: 0, top: window.innerHeight * 0.4, behavior: "instant"});
-            break;
-        case scrollUpHalf:
-            window.scrollBy({left: 0, top: -window.innerHeight * 0.4, behavior: "instant"});
-            break;
-        case downSmall:
-            window.scrollBy({left: 0, top: window.innerHeight * 0.1, behavior: "instant"});
-            break;
-        case upSmall:
-            window.scrollBy({left: 0, top: -window.innerHeight * 0.1, behavior: "instant"});
-            break;
-        case jumpTop:
-            window.scrollTo({left: 0, top: 0, behavior: "instant"});
-            break;
-        case jumpBottom:
-            window.scrollTo({left: 0, top: document.body.scrollHeight, behavior: "instant"});
-            break;
-        case reload:
-            location.reload();
-            break;
-        case close:
-            window.close();
-            break;
-        case copyURL:
-            navigator.clipboard.writeText(window.location.href);
-            break;
-        case goBack:
-            history.back();
-            break;
-        case goForward:
-            history.forward();
-            break;
-        case upURL:
-            window.location.href = window.location.href.replace(/\/[^\/]+\/?$/, '');
-            break;
-        case focusInput:
-            e.preventDefault();
-            var input = document.querySelector('input, textarea');
-            if (input) {
-                input.focus();
-            }
-            break;
-        case slower:
-            document.querySelector('video').playbackRate-=0.25
-            break;
-        case faster:
-            document.querySelector('video').playbackRate+=0.25
-            break;
-        case normalSpeed:
-            document.querySelector('video').playbackRate=1
-            break;
-        case prevSection:
-            (function() {
-                var prevTargetElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, hr, section, article, aside, nav, header, main');
-                var currentY = window.scrollY;
-                for (var i = prevTargetElements.length - 1; i >= 0; i--) {
-                    var pElementY = prevTargetElements[i].getBoundingClientRect().top + window.scrollY;
-                    if (pElementY < currentY) {
-                        window.scrollTo({ top: pElementY, behavior: 'smooth' });
-                        break;
-                    }
-                }
-            })();
-            break;
-        case nextSection:
-            (function() {
-                var nextTargetElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, hr, section, article, aside, nav, header, main');
-                var currentY = window.scrollY;
-                for (var i = 0; i < nextTargetElements.length; i++) {
-                    var nElementY = nextTargetElements[i].getBoundingClientRect().top + window.scrollY;
-                    if (nElementY > (currentY+1)) {
-                        window.scrollTo({ top: nElementY, behavior: 'smooth', block: 'start' });
-                        break;
-                    }
-                }
-            })();
-            break;
+
+    let key = e.key;
+    if (actions[key]) {
+        actions[key]();
+        lastAction = key;
     }
 });
 
+document.addEventListener('keydown', function(e) {
+    if (e.key === repeatAction) {
+        e.preventDefault();
+        if (lastAction && actions[lastAction]) {
+           actions[lastAction]();
+        }
+    }
+});
