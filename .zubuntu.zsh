cdd46c95e62000f19c905df98a7db4d4920e8f9c
Tanath <3394708+Tanath@users.noreply.github.com>
2024-02-11 16:46:26 -0500

    Add comments for alias mnemonics in ubuntu types.

--- .zubuntu.zsh
+++ .zubuntu.zsh
@@ -1,9 +1,16 @@
+#dpl (DPkg List package files)
 alias dpl='dpkg -L'
+#dqs (Dpkg-Query Search) to find what owns a file and stuff.
 alias dqs='dpkg-query -S'
+#ain (Apt INstall)
 alias ain='sudo apt install'
+#arm (Apt ReMove)
 alias arm='sudo apt remove'
+#ash (Apt SHow)
 alias ash='apt show'
+#aar (Apt AutoRemove)
 alias aar='sudo apt autoremove'
+#bin (show BINaries from package)
 bin () { dpkg -L "$@" | grep bin/ }
+#asl (Apt-cache Search, no Libs)
 asl () { apt-cache search "$@" | grep -v '^lib' }
-
