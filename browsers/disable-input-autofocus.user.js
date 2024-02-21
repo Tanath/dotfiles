6838ed56ede37fb38850039bde1ad40a8970fce4
Tanath <3394708+Tanath@users.noreply.github.com>
2024-02-21 17:17:50 -0500

    Add input blur override to autofocus disabling userscript.

--- browsers/disable-input-autofocus.user.js
+++ browsers/disable-input-autofocus.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name        Disable input autofocus
 // @match       https://*/*
-// @grant       none
-// @version     0.1
+// @grant       GM_addStyle
+// @version     0.2
 // @author      Tanath
 // @description Prevent pages from automatically focusing input.
 // ==/UserScript==
 
+GM_addStyle("input { focus: blur !important; }");
 var inputList = document.getElementsByTagName("input");
 for(var i = 0; i < inputList.length; i++){
     inputList[i].blur();
