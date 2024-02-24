07066d3d5fb6460ef546b355ff501da5471de053
Tanath <3394708+Tanath@users.noreply.github.com>
2024-02-24 00:44:26 -0500

    Rewrite userscript for setting fonts.
    Set sans and mono separately. Use Fira Code nerd font for mono.  Apply fonts to text tags only.

--- browsers/Set font to Atkinson Hyperlegible.user.js
+++ browsers/Set font to Atkinson Hyperlegible.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Set font to Atkinson Hyperlegible
-// @version      0.9.1
-// @description  Set font to Atkinson Hyperlegible (Pro).
+// @name         Set fonts to Atkinson Hyperlegible Pro & Fira Code Nerd Font
+// @version      1.0
+// @description  Set sans font to Atkinson Hyperlegible Pro & mono to Fira Code Nerd Font.
 // @author       Tanath
 // @downloadURL  https://github.com/Tanath/dotfiles/raw/master/browsers/Set%20font%20to%20Atkinson%20Hyperlegible.user.js
 // @updateURL    https://github.com/Tanath/dotfiles/raw/master/browsers/Set%20font%20to%20Atkinson%20Hyperlegible.user.js
@@ -10,50 +10,80 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-window.onload = function() {
-    'use strict';
-    var link1 = document.createElement('link');
-    link1.href = 'https://github.com';
-    link1.rel = 'preconnect';
-    document.head.appendChild(link1);
+document.onreadystatechange = function () {
+    if (document.readyState === 'complete') {
+        'use strict';
 
-    var link2 = document.createElement('link');
-    link2.href = 'https://fonts.googleapis.com';
-    link2.rel = 'preconnect';
-    document.head.appendChild(link2);
+        var link1 = document.createElement('link');
+        link1.href = 'https://github.com';
+        link1.rel = 'preconnect';
+        document.head.appendChild(link1);
 
-    var link3 = document.createElement('link');
-    link3.href = 'https://fonts.googleapis.com/css2?display=swap&family=Atkinson+Hyperlegible&family=Source+Code+Pro&family=Fira+Code&family=Noto+Sans+Symbols';
-    link3.rel = 'stylesheet';
-    document.head.appendChild(link3);
-
-    var fontFace = `
+        var fontFace = `
         @font-face {
-            font-family: 'Atkinson Hyperlegible Pro';
-            src: url('https://github.com/jacobxperez/atkinson-hyperlegible-pro/raw/main/fonts/web/AtkinsonHyperPro-Regular.woff2') format('woff2'),
-                 url('https://github.com/jacobxperez/atkinson-hyperlegible-pro/raw/main/fonts/web/AtkinsonHyperPro-Bold.woff2') format('woff2'),
-                 url('https://github.com/jacobxperez/atkinson-hyperlegible-pro/raw/main/fonts/web/AtkinsonHyperPro-Italic.woff2') format('woff2'),
-                 url('https://github.com/jacobxperez/atkinson-hyperlegible-pro/raw/main/fonts/web/AtkinsonHyperPro-BoldItalic.woff2') format('woff2');
+            font-family: 'Atkinson Hyperlegible Pro', sans-serif;
+            src: url('https://github.com/jacobxperez/atkinson-hyperlegible-pro/raw/v1.5.1/fonts/web/AtkinsonHyperPro-Regular.woff2') format('woff2'),
+                 url('https://github.com/jacobxperez/atkinson-hyperlegible-pro/raw/v1.5.1/fonts/web/AtkinsonHyperPro-Bold.woff2') format('woff2'),
+                 url('https://github.com/jacobxperez/atkinson-hyperlegible-pro/raw/v1.5.1/fonts/web/AtkinsonHyperPro-Italic.woff2') format('woff2'),
+                 url('https://github.com/jacobxperez/atkinson-hyperlegible-pro/raw/v1.5.1/fonts/web/AtkinsonHyperPro-BoldItalic.woff2') format('woff2');
             font-weight: 400 700;
-            font-size: 100%;
             font-style: normal italic;
-            font-variant-numeric: slashed-zero;
-            font-variant-ligatures: no-common-ligatures;
         }
-    `;
-    var styleElement = document.createElement('style');
-    styleElement.appendChild(document.createTextNode(fontFace));
-    document.head.appendChild(styleElement);
 
-    var style = `
-        * {
-            font-family: 'Atkinson Hyperlegible Pro', 'Atkinson Hyperlegible', 'Source Code Pro', 'Fira Code', 'Noto Sans Symbols', sans-serif;
+        @font-face {
+            font-family: 'FiraCode Nerd Font Mono', monospace;
+            font-style: normal;
+            font-weight: 300;
+            src: url('https://github.com/ryanoasis/nerd-fonts/raw/v3.1.1/patched-fonts/FiraCode/Light/FiraCodeNerdFontMono-Light.ttf') format('truetype');
+            font-variant-ligatures: none;
         }
-    `;
-    var styleElement2 = document.createElement('style');
-    styleElement2.appendChild(document.createTextNode(style));
-    document.head.appendChild(styleElement2);
 
-    GM_addStyle(style);
+        @font-face {
+            font-family: 'FiraCode Nerd Font Mono', monospace;
+            font-style: normal;
+            font-weight: 400;
+            src: url('https://github.com/ryanoasis/nerd-fonts/raw/v3.1.1/patched-fonts/FiraCode/Retina/FiraCodeNerdFontMono-Retina.ttf') format('truetype');
+            font-variant-ligatures: none;
+        }
+
+        @font-face {
+            font-family: 'FiraCode Nerd Font Mono', monospace;
+            font-style: normal;
+            font-weight: 500;
+            src: url('https://github.com/ryanoasis/nerd-fonts/raw/v3.1.1/patched-fonts/FiraCode/Medium/FiraCodeNerdFontMono-Medium.ttf') format('truetype');
+            font-variant-ligatures: none;
+        }
+
+        @font-face {
+            font-family: 'FiraCode Nerd Font Mono', monospace;
+            font-weight: 600;
+            src: url('https://github.com/ryanoasis/nerd-fonts/raw/v3.1.1/patched-fonts/FiraCode/SemiBold/FiraCodeNerdFontMono-SemiBold.ttf') format('truetype');
+            font-variant-ligatures: none;
+        }
+
+        @font-face {
+            font-family: 'FiraCode Nerd Font Mono', monospace;
+            font-weight: 700;
+            src: url('https://github.com/ryanoasis/nerd-fonts/raw/v3.1.1/patched-fonts/FiraCode/Bold/FiraCodeNerdFontMono-Bold.ttf') format('truetype');
+            font-variant-ligatures: none;
+        }
+        `;
+
+        var styleElement = document.createElement('style');
+        styleElement.appendChild(document.createTextNode(fontFace));
+        document.head.appendChild(styleElement);
+
+        var style = `
+        body, h1, h2, h3, h4, h5, h6, p, section, article, aside, nav, header, main, q, li, blockquote {
+            font-family: 'Atkinson Hyperlegible Pro', sans-serif;
+        }
+        code, pre, kbd, var, samp {
+            font-family: 'FiraCode Nerd Font Mono', monospace;
+        }
+        `;
+        var styleElement2 = document.createElement('style');
+        styleElement2.appendChild(document.createTextNode(style));
+        document.head.appendChild(styleElement2);
+    };
 };
 
